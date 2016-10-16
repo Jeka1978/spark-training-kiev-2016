57697dee528719abe57ee744c0447cc6878f2903
bsevgeny@gmail.com <papakita2009>
2016-10-16 15:27:25 +0300

    fix error on windows

--- src/main/scala/taxi/Main.scala
+++ src/main/scala/taxi/Main.scala
@@ -8,6 +8,8 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object Main {
   def main(args: Array[String]): Unit = {
+    //https://codeload.github.com/srccodes/hadoop-common-2.2.0-bin/zip/master
+    System.setProperty("hadoop.home.dir", "C:\\util\\hadoop-common-2.2.0-bin-master\\")
     val conf: SparkConf = new SparkConf().setAppName("taxi").setMaster("local")
     val context: SparkContext = new SparkContext(conf)
     val rdd: RDD[String] = context.textFile("data/taxi/trips.txt")
